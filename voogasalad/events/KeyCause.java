b59c020404c6caf55e487ec8b3471e8974395785
Anita Desai <anita.desai96@gmail.com>
2016-03-31 16:55:31 -0400

    Added an events package that contains basic classes for causes/effects

---
+++ voogasalad/events/KeyCause.java
@@ -0,0 +1,13 @@
+package events;
+
+/*
+ * This class extends cause to create the specificity needed to address key causes. This will allow us to deal with
+ * potential listeners and other complications that are specific to key strokes.
+ */
+public class KeyCause extends Cause {
+
+	public KeyCause() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
