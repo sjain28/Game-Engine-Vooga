113cd374cf1cd9e97f7640fae8ad8d41fc7fc77a
Anita Desai <anita.desai96@gmail.com>
2016-03-31 16:30:12 -0400

    GameEngine basic classes added with comments for purpose

---
+++ voogasalad/GameEngine/Sprite.java
@@ -0,0 +1,22 @@
+package GameEngine;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Sprite extends ImageView {
+
+	public Sprite() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Sprite(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Sprite(Image arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
