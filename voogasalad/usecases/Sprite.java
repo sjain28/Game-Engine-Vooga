353e1a88c7ce7c8be560a9dd369c756c2ba6690d
Anita Desai <anita.desai96@gmail.com>
2016-04-01 00:39:22 -0400

    Added sprite use case

---
+++ voogasalad/usecases/Sprite.java
@@ -0,0 +1,47 @@
+package usecases;
+
+import java.util.HashMap;
+import java.util.Map;
+import authoring.interfaces.Moveable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import tools.Vector;
+import tools.interfaces.*;
+
+public class Sprite extends ImageView implements Moveable {
+
+	private Vector myVelocity;
+	private String myID;
+	private Map<String, VoogaData> myProperties;
+
+	public Sprite(String imageID, String id) {
+		super(new Image(imageID));
+		myID = id;
+		myProperties = new HashMap<String, VoogaData>();
+	}
+	public void addParameter(String parameter, VoogaData value){
+		myProperties.put(parameter, value);
+	}
+	@Override
+	public Vector getVelocity() {
+		return myVelocity;
+	}
+
+	@Override
+	public void setVelocity(Vector v) {
+		myVelocity = v;
+	}
+
+	public void addProperty(String s, VoogaData v){
+		myProperties.put(s, v);
+	}
+
+	public VoogaData getProperty(String s){
+		return myProperties.get(s);
+	}
+
+	public String getID(){
+		return myID;
+	}
+
+}
