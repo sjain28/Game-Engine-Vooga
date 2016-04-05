f3dcd1724d53058da356c3c3eea21052eee372cd
Joshua Xu <jx35@duke.edu>
2016-04-05 16:17:32 -0400

    minor change

---
+++ voogasalad/authoring/model/VoogaButton.java
@@ -0,0 +1,13 @@
+package authoring.model;
+
+import authoring.interfaces.Elementable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class VoogaButton extends Button implements Elementable {
+
+	public VoogaButton() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
