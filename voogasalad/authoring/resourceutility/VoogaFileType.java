fbfeddd94485d9bb8adf7f946f103f4c771da906
Aditya Srinivasan <as577@duke.edu>
2016-04-04 23:16:35 -0400

    Renaming supported for Resource Utility

--- voogasalad/authoring/resourceutility/VoogaFileType.java
+++ voogasalad/authoring/resourceutility/VoogaFileType.java
@@ -1,17 +1,10 @@
 package authoring.resourceutility;
-import javafx.scene.image.Image;
 
 public enum VoogaFileType {
 	FOLDER, IMAGE, AUDIO;
 	
-	private Image thumbnail;
-		
 	private VoogaFileType() {
-		this.thumbnail = new Image("file:resources/" + this.name() + ".png");
-	}
-	
-	public Image getFileThumbnail() {
-		return this.thumbnail;
+		
 	}
 
 }
