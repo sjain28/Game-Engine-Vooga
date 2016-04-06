07900bc072a922b6ce7c58f267522edd42f0da88
NickLockett <NickS.Lockett@gmail.com>
2016-04-06 00:11:15 -0400

    Authoring UIPlaceholders, small bug fixes, and sizing adjustments

---
+++ voogasalad/authoring/gui/UIConstants.java
@@ -0,0 +1,46 @@
+package authoring.gui;
+/*
+ * Constants for building out UIManager.
+ * 
+ * Since we are employing Mosaic, all values are percentages.
+ */
+
+public class UIConstants {
+    
+    public static final double MENUBAR_X = 0;
+    public static final double MENUBAR_Y = 1;
+    public static final double MENUBAR_WIDTH = 1;
+    public static final double MENUBAR_HEIGHT = 0.05;
+    public static final String MENUBAR_ID = "MenuBar";
+    
+    public static final double TOOLBAR_X = 0;
+    public static final double TOOLBAR_Y = 0.05;
+    public static final double TOOLBAR_WIDTH = 1;
+    public static final double TOOLBAR_HEIGHT = 0.05;
+    public static final String TOOLBAR_ID = "ToolBar";
+    
+    public static final double EXPLORE_X = 0;
+    public static final double EXPLORE_Y = 0.30;
+    public static final double EXPLORE_WIDTH = 0.25;
+    public static final double EXPLORE_HEIGHT = 0.30;
+    public static final String EXPLORE_ID = "explore";
+    
+    public static final double CHARACTERISTICS_X = 0;
+    public static final double CHARACTERISTICS_Y = 0.60;
+    public static final double CHARACTERISTICS_WIDTH = 0.25;
+    public static final double CHARACTERISTICS_HEIGHT = 0.30;
+    public static final String CHARACTERISTICS_ID = "char";
+    
+    public static final double EVENTS_X = 0;
+    public static final double EVENTS_Y = 1;
+    public static final double EVENTS_WIDTH = 0.25;
+    public static final double EVENTS_HEIGHT = 0.30;
+    public static final String EVENTS_ID = "events";
+    
+    public static final double BOARD_X = 0.25;
+    public static final double BOARD_Y = 0;
+    public static final double BOARD_WIDTH = 0.75;
+    public static final double BOARD_HEIGHT = 0.90;
+    public static final String BOARD_ID = "board";
+   
+}
