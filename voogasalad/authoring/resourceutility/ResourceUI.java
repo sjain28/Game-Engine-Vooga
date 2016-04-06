78d17e05206ee93fb0bf1b974b9c5c82b57aa579
Aditya Srinivasan <as577@duke.edu>
2016-04-06 01:09:02 -0400

    Completed version 1.0 of resource utility

--- voogasalad/authoring/resourceutility/ResourceUI.java
+++ voogasalad/authoring/resourceutility/ResourceUI.java
@@ -1,16 +1,38 @@
 package authoring.resourceutility;
+
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+/**
+ * The class initialized by users of this utility. It is a VBox containing the
+ * TreeView structure of folders and files, with buttons to create new folders
+ * and import new files.
+ * @author DoovalSalad
+ *
+ */
 public class ResourceUI extends VBox {
 	
+	/**
+	 * Private instance variables
+	 */
 	private HBox buttonContainer;
 	private ResourceTreeView rtv;
 	
+	/**
+	 * Constants
+	 */
+	private static final String DEFAULT_PROJECT_NAME = "My Project";
+	private static final String ADD_FOLDER_PROMPT = "Add Folder";
+	private static final String IMPORT_FILE_PROMPT = "Import File";
+	private static final double SPACING = 10;
+
+	/**
+	 * The constructor, which initializes the nodes that make up the entire UI.
+	 */
 	public ResourceUI() {
-		buttonContainer = new HBox();
+		buttonContainer = new HBox(SPACING);
 		
-		rtv = new ResourceTreeView(new VoogaFile(VoogaFileType.FOLDER, "My Project"));
+		rtv = new ResourceTreeView(new VoogaFile(VoogaFileType.FOLDER, DEFAULT_PROJECT_NAME));
 	
 		this.getChildren().addAll(rtv, buttonContainer);
 		
@@ -18,27 +40,28 @@ public class ResourceUI extends VBox {
 		
 	}
 	
+	/**
+	 * Makes buttons to add folders and import files.
+	 */
 	private void makeAddButtons() {
 		ButtonMaker maker = new ButtonMaker();
-		this.buttonContainer.getChildren().addAll(maker.makeButton("Add Folder", e -> promptFolderName()),
-												  maker.makeButton("Import File", e -> importFile()));
+		this.buttonContainer.getChildren().addAll(maker.makeButton(ADD_FOLDER_PROMPT, e -> promptFolderName()),
+												  maker.makeButton(IMPORT_FILE_PROMPT, e -> importFile()));
 		
 	}
 	
+	/**
+	 * Opens up the NamePrompter for adding folders.
+	 */
 	private void promptFolderName() {
-		new NamePrompter(this);
+		new NamePrompter(rtv);
 	}
 	
+	/**
+	 * Opens the FileImporter for importing files.
+	 */
 	private void importFile() {
-		new FileImporter(this);
-	}
-	
-	public void addFolder(String folderName) {
-		rtv.addItem(new VoogaFile(VoogaFileType.FOLDER, folderName));
+		new FileImporter(rtv);
 	}
 
-	public void addItem(VoogaFile voogaFile) {
-		rtv.addItem(voogaFile);
-	}
-	
 }
---
+++ voogasalad/authoring/resourceutility/TreeIterator.java
@@ -0,0 +1,46 @@
+package authoring.resourceutility;
+
+import java.util.Iterator;
+import java.util.Stack;
+import javafx.scene.control.TreeItem;
+
+/**
+ * An iterator to iterate through the items in the TreeView
+ * @author DoovalSalad
+ *
+ * @param <T> the parameterization of the TreeView (in this case, VoogaFile)
+ */
+public class TreeIterator<T> implements Iterator<TreeItem<T>> {
+	
+	/**
+	 * Private instance stack
+	 */
+    private Stack<TreeItem<T>> stack = new Stack<>();
+
+    /**
+     * Initializes the stack with the root of the tree.
+     * @param root
+     */
+    public TreeIterator(TreeItem<T> root) {
+        stack.push(root);
+    }
+
+    /**
+     * Determines whether the stack has more items.
+     */
+    @Override
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /**
+     * Returns the next item from the stack.
+     */
+    @Override
+    public TreeItem<T> next() {
+        TreeItem<T> nextItem = stack.pop();
+        nextItem.getChildren().forEach(stack::push);
+
+        return nextItem;
+    }
+}
