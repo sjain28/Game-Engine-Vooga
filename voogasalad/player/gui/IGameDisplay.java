4917b949cc6ad09bd987ad3146556f91b81d925d
Hunter Lee <narendly@gmail.com>
2016-04-07 14:49:12 -0400

    Control added to StandardDisplay at the bottom

--- voogasalad/player/gui/IGameDisplay.java
+++ voogasalad/player/gui/IGameDisplay.java
@@ -10,12 +10,18 @@ public interface IGameDisplay {
 	
 	IPromptFactory myPromptFactory = new PromptFactory();
 	
+//	IPromptFactory getPromptFactory();
+	
 	IControl myControl = new StandardControl();
+	
+//	IControl getControl();
 	
 	void read();
 	
 	void display();
 	
 	void createPrompt(String message);
+	
+
 	
 }
