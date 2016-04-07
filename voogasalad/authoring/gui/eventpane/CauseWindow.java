97a6a0b9eb8fda8df4f99dc4012c60376a2ae4c7
Arjun Desai <arjunaashna@gmail.com>
2016-04-07 13:27:42 -0400

    Updated events display

--- voogasalad/authoring/gui/eventpane/CauseWindow.java
+++ voogasalad/authoring/gui/eventpane/CauseWindow.java
@@ -1,5 +1,6 @@
 package authoring.gui.eventpane;
 
+import java.util.ResourceBundle;
 import events.Cause;
 import events.Event;
 import javafx.scene.layout.GridPane;
@@ -7,9 +8,13 @@ import javafx.scene.layout.GridPane;
 public class CauseWindow extends GridPane {
     Event event;
     Cause cause;
+    ResourceBundle causes;
     
     public CauseWindow(Event e){
         event = e;
+        causes = ResourceBundle.getBundle("/events/CauseTypes");
     }
+    
+    
 
 }
