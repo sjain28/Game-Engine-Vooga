740c53e9436c56675535cbcbafb8504ba2cb9012
Arjun Desai <arjunaashna@gmail.com>
2016-04-07 11:16:58 -0400

    Updated variable effect

--- voogasalad/gameengine/SpriteFactory.java
+++ voogasalad/gameengine/SpriteFactory.java
@@ -12,12 +12,6 @@ import tools.interfaces.VoogaData;
  * @author Krista
  *
  */
-
-//TODO: 
-//Add method to create a new Archetype
-//Saving to library
-//Importing from library
-
 public class SpriteFactory {
 
 	private HashMap<String,Sprite> myArchetypes; 
@@ -25,7 +19,6 @@ public class SpriteFactory {
 	public SpriteFactory() {
 		myArchetypes = new HashMap<String,Sprite>();
 	}
-	
 	/**
 	 * Create a completely new Sprite of a given archetype
 	 * @param archetype
@@ -34,7 +27,8 @@ public class SpriteFactory {
 	public Sprite createSprite(String archetype){
 		Sprite toCopy = myArchetypes.get(archetype);
 		Map<String, VoogaData> newProperties = new HashMap<String, VoogaData>(toCopy.getParameterMap());
-		return new Sprite(toCopy.getImagePath(), archetype, newProperties);
+		Sprite clone = new Sprite(toCopy.getImagePath(), archetype,newProperties);
+		return clone;
 	}
 	
 	public void setArchetype(String archetype, Sprite s){
