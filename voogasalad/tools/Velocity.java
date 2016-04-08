76c3b8ed73932c5f64a9921fc78e01fd239389f6
Hunter <narendly@gmail.com>
2016-04-07 23:10:53 -0400

    Position and Velocity fully implemented

--- voogasalad/tools/Velocity.java
+++ voogasalad/tools/Velocity.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package tools;
 
 /**
@@ -25,18 +22,57 @@ public class Velocity extends Vector {
 	/**
 	 * Computes and returns the magnitude of the velocity
 	 * 
+	 * @return magnitude
 	 */
 	public double getMagnitude() {
-		return 0;
+		return Math.sqrt(Math.pow(getX(), 2) + Math.pow(getY(), 2));
 	}
 	
 	/**
 	 * Computes and returns the direction of the velocity
+	 * Based upon the traditional Cartesian coordinate system
 	 * 
-	 * @return
+	 * Direction ranges from 0 to 360
+	 * 
+	 * @return direction
 	 */
-	public double getDirectionDegree() {
-		return 0;
+	public double getAngleDegree() {
+		double angle = 180.0 / Math.PI * Math.atan2(getY(), getX());
+		return (angle > 0) ? angle : angle + 360.0;
+	}
+	
+	/**
+	 * Computes and returns the direction of the velocity
+	 * Based upon the traditional Cartesian coordinate system
+	 * 
+	 * Direction is in radians
+	 * 
+	 * @return direction
+	 */
+	public double getAngleRadian() {
+		double angle = Math.atan2(getY(), getX());
+		return (angle > 0) ? angle : angle + 2 * Math.PI;
+	}
+	
+	/**
+	 * For unit testing only
+	 * 
+	 */
+	public static void main(String args[]){
+		
+		Velocity v1 = new Velocity(1, 1);
+		Velocity v2 = new Velocity(1, 0);
+		Velocity v3 = new Velocity(-1, -1);
+		
+		System.out.println("v1's Magnitude: " + v1.getMagnitude());
+		System.out.println("v1's Direction angle: " + v1.getAngleDegree());
+		
+		System.out.println("v2's Magnitude: " + v2.getMagnitude());
+		System.out.println("v2's Direction angle: " + v2.getAngleDegree());
+		
+		System.out.println("v3's Magnitude: " + v3.getMagnitude());
+		System.out.println("v3's Direction angle: " + v3.getAngleDegree());
+		
 	}
 
 }
