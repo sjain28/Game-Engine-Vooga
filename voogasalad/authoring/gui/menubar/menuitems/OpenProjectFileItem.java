ceb7c4b7c6a1c07a8648474622a59a81a7bfe32c
NickLockett <NickS.Lockett@gmail.com>
2016-04-09 17:25:33 -0400

    open file frontend implementaion

--- voogasalad/authoring/gui/menubar/menuitems/OpenProjectFileItem.java
+++ voogasalad/authoring/gui/menubar/menuitems/OpenProjectFileItem.java
@@ -1,8 +1,13 @@
 package authoring.gui.menubar.menuitems;
 
+import java.io.File;
+import application.Launcher;
 import authoring.gui.menubar.MenuItemHandler;
 import authoring.interfaces.model.CompleteAuthoringModelable;
 import authoring.interfaces.model.Savable;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
 public class OpenProjectFileItem extends MenuItemHandler {
     private Savable myManager;
@@ -14,8 +19,15 @@ public class OpenProjectFileItem extends MenuItemHandler {
 
 	@Override
 	public void handle() {
-		// TODO Auto-generated method stub
-		
+	    FileChooser popUp = new FileChooser();
+	    popUp.setTitle("Open New Project File");
+	    popUp.getExtensionFilters().addAll(
+	                                             //TODO: add in appropriate file extension filter
+	                                             new ExtensionFilter("All Files", "*.*"));
+	    File file = popUp.showOpenDialog(new Stage());
+	    if(file != null){
+	        Launcher newProject = new Launcher(new Stage(), file);
+	    };
 	}
 
 }
