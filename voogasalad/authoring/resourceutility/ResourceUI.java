d07894a089011af8a67c32b713f699f118edc8e6
Aditya Srinivasan <as577@duke.edu>
2016-04-11 21:31:15 -0400

    Implemented positioning of dropped assets and exceptions thrown for invalid file imports

--- voogasalad/authoring/resourceutility/ResourceUI.java
+++ voogasalad/authoring/resourceutility/ResourceUI.java
@@ -1,5 +1,7 @@
 package authoring.resourceutility;
 
+import auxiliary.VoogaAlert;
+import auxiliary.VoogaException;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.HBox;
@@ -9,18 +11,19 @@ import javafx.scene.layout.VBox;
  * The class initialized by users of this utility. It is a VBox containing the
  * TreeView structure of folders and files, with buttons to create new folders
  * and import new files.
+ * 
  * @author DoovalSalad
  *
  */
 public class ResourceUI extends TabPane {
-	
+
 	/**
 	 * Private instance variables
 	 */
 	private HBox buttonContainer;
 	private ResourceTreeView rtv;
 	private VBox container;
-	
+
 	/**
 	 * Constants
 	 */
@@ -36,43 +39,52 @@ public class ResourceUI extends TabPane {
 	public ResourceUI() {
 		container = new VBox(SPACING);
 		buttonContainer = new HBox(SPACING);
-		
+
 		rtv = new ResourceTreeView(new VoogaFile(VoogaFileType.FOLDER, DEFAULT_PROJECT_NAME));
-	
+
 		container.getChildren().addAll(rtv, buttonContainer);
-		
-		makeAddButtons();
-		
+
+		try {
+			makeAddButtons();
+		} catch(VoogaException e) {
+			System.out.println("caught");
+		}
+
 		Tab holder = new Tab(WINDOW_NAME);
 		holder.setContent(container);
 		this.getTabs().add(holder);
-		
+
 	}
-	
+
 	/**
 	 * Makes buttons to add folders and import files.
 	 */
 	private void makeAddButtons() {
 		ButtonMaker maker = new ButtonMaker();
 		this.buttonContainer.getChildren().addAll(maker.makeButton(ADD_FOLDER_PROMPT, e -> promptFolderName()),
-												  maker.makeButton(IMPORT_FILE_PROMPT, e -> importFile()));
-		
+				maker.makeButton(IMPORT_FILE_PROMPT, e -> {
+					try {
+						importFile();
+					} catch(VoogaException ee) {
+						new VoogaAlert(ee.getMessage());
+					}
+				}));
 	}
-	
+
 	/**
 	 * Opens up the NamePrompter for adding folders.
 	 */
 	private void promptFolderName() {
 		new NamePrompter(rtv);
 	}
-	
+
 	/**
 	 * Opens the FileImporter for importing files.
 	 */
-	private void importFile() {
+	private void importFile() throws VoogaException {
 		new FileImporter(rtv);
 	}
-	
+
 	public ResourceTreeView getRTV() {
 		return this.rtv;
 	}
