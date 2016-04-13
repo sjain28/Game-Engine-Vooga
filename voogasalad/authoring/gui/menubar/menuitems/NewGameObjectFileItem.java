ba0041b7798bf8bd208d0cf0b2d94c7f7d7d04d8
NickLockett <NickS.Lockett@gmail.com>
2016-04-13 00:05:43 -0400

    partial commenting of MenuItems

--- voogasalad/authoring/gui/menubar/menuitems/NewGameObjectFileItem.java
+++ voogasalad/authoring/gui/menubar/menuitems/NewGameObjectFileItem.java
@@ -12,12 +12,19 @@ import javafx.stage.Stage;
 
 public class NewGameObjectFileItem extends MenuItemHandler {
         EditElementable myManager;
-	
+        /**
+         * Initializes the MenuItem
+         * 
+         * @param model to interface backend interactions with the model
+         * @param event: Unused vestige of previous poor programming. Should soon be phased out.
+         */
 	public NewGameObjectFileItem(CompleteAuthoringModelable model, EventHandler<InputEvent> event) {
 		super();
 		myManager = model;
 	}
-
+	/**
+         * Action to be taken on the selection of this menuItem
+         */
 	@Override
 	public void handle() {
 	    Stage popup = new Stage();
