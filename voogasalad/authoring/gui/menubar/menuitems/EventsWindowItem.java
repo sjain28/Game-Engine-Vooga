980a29fe590a47b8aa38fdbfe7e1fe20b004bacc
NickLockett <NickS.Lockett@gmail.com>
2016-04-13 00:05:43 -0400

    partial commenting of MenuItems

--- voogasalad/authoring/gui/menubar/menuitems/EventsWindowItem.java
+++ voogasalad/authoring/gui/menubar/menuitems/EventsWindowItem.java
@@ -4,13 +4,24 @@ import authoring.gui.menubar.MenuItemHandler;
 import authoring.interfaces.model.CompleteAuthoringModelable;
 import javafx.event.EventHandler;
 import javafx.scene.input.InputEvent;
-
+/**
+ * MenuItem to Select the Event Window
+ * @author Nick
+ *
+ */
 public class EventsWindowItem extends MenuItemHandler {
-	
+    /**
+     * Initializes the MenuItem
+     * 
+     * @param model to interface backend interactions with the model
+     * @param event: Unused vestige of previous poor programming. Should soon be phased out.
+     */
 	public EventsWindowItem(CompleteAuthoringModelable model, EventHandler<InputEvent> event) {
 		super();
 	}
-
+	/**
+         * Action to be taken on the selection of this menuItem
+         */
 	@Override
 	public void handle() {
 		// TODO Auto-generated method stub
--- voogasalad/authoring/gui/menubar/menuitems/NewProjectFileItem.java
+++ voogasalad/authoring/gui/menubar/menuitems/NewProjectFileItem.java
@@ -10,12 +10,19 @@ import javafx.stage.Stage;
 
 public class NewProjectFileItem extends MenuItemHandler {
     private Saveable myManager;
-	
+    /**
+     * Initializes the MenuItem
+     * 
+     * @param model to interface backend interactions with the model
+     * @param event: Unused vestige of previous poor programming. Should soon be phased out.
+     */
 	public NewProjectFileItem(CompleteAuthoringModelable model, EventHandler<InputEvent> event) {
 		super();
 		myManager = model;
 	}
-
+	/**
+         * Action to be taken on the selection of this menuItem
+         */
 	@Override
 	public void handle() {
 		new Launcher(new Stage());
