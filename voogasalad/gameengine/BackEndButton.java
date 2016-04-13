1b453124416d77eb3fc7eb763fe742c5610fb30a
Joshua Xu <jx35@duke.edu>
2016-04-12 21:12:11 -0400

    added backend button and text

--- voogasalad/gameengine/BackEndButton.java
+++ voogasalad/gameengine/BackEndButton.java
@@ -1,12 +1,52 @@
 package gameengine;
 
+import java.util.Map;
+
+import authoring.interfaces.Elementable;
 import data.Displayable;
 import javafx.scene.Node;
+import tools.interfaces.VoogaData;
 
-public class BackEndButton implements Displayable {
+public class BackEndButton implements Elementable {
 
 	@Override
 	public Node getNodeObject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<String, VoogaData> getVoogaProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addProperty(String name, VoogaData data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeProperty(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getID() {
 		// TODO Auto-generated method stub
 		return null;
 	}
