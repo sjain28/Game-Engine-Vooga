530115c97d02464057823ad334a4fcbf4dfe03c0
Joshua Xu <jx35@duke.edu>
2016-04-12 20:16:47 -0400

    josh created back end text

--- voogasalad/data/VoogaTextWritingTest.java
+++ voogasalad/data/VoogaTextWritingTest.java
@@ -1,19 +1,26 @@
 package data;
 
+import authoring.model.VoogaButton;
 import authoring.model.VoogaText;
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class VoogaTextWritingTest extends Application {
 
+	private static final String testString= "levels/VoogaTextWriting.xml";
+	
     @Override
     public void start (Stage primaryStage) throws Exception {
         VoogaText vt = new VoogaText();
         vt.setText("DASFASFAF");
         
-        Serializer.serialize(vt, "levels/Test.xml");
-        Object o = DeSerializer.deserialize(1, "levels/Test.xml").get(0);
-        VoogaText vt2 = (VoogaText) o;
+        Rectangle vb = new Rectangle(3,4);
+
+        Serializer.serialize(vb, testString);
+        Object o = DeSerializer.deserialize(1, "levels/VoogaTextWriting.xml").get(0);
+        Rectangle vt2 = (Rectangle) o;
         System.out.println("VoogaText");
         System.out.println(vt2);
     }
---
+++ voogasalad/gameengine/BackEndButton.java
@@ -0,0 +1,14 @@
+package gameengine;
+
+import data.Displayable;
+import javafx.scene.Node;
+
+public class BackEndButton implements Displayable {
+
+	@Override
+	public Node getNodeObject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
