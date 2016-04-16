a4494d7a1e04467e7a6d4fa6fefd5f2d76d90abf
Hunter Lee <narendly@gmail.com>
2016-04-16 11:03:14 -0400

    GlobalVariableManager fleshed out

--- voogasalad/player/leveldatamanager/GlobalVariableManager.java
+++ voogasalad/player/leveldatamanager/GlobalVariableManager.java
@@ -3,10 +3,41 @@
  */
 package player.leveldatamanager;
 
+import java.util.Map;
+
+import tools.interfaces.VoogaData;
+
 /**
+ * Global Variable manager that keeps track of global variables
+ * separately from Elementable objects and Events
+ * 
  * @author Hunter Lee
  *
  */
 public class GlobalVariableManager {
+
+	private ObjectManager myObjectManager;
+	private Map<String, VoogaData> myGlobalVariables;
+	
+	public GlobalVariableManager(ObjectManager myObjectManager, 
+			Map<String, VoogaData> variableObjects) {
+		// TODO Auto-generated constructor stub
+		this.myObjectManager = myObjectManager;
+		this.myGlobalVariables = variableObjects;
+	}
+
+	/**
+	 * @return the myObjectManager
+	 */
+	public ObjectManager getObjectManager() {
+		return myObjectManager;
+	}
+
+	/**
+	 * @return the myGlobalVariables
+	 */
+	public VoogaData getVariable(String name) {
+		return myGlobalVariables.get(name);
+	}
 
 }
