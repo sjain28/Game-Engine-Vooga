8b07f60c56ef6d7a93ff2e0b50eab63952d8adcf
Joshua Xu <jx35@duke.edu>
2016-04-17 19:57:44 -0400

    -temp changes

---
+++ voogasalad/player/leveldatamanager/LevelTransitioner.java
@@ -0,0 +1,27 @@
+package player.leveldatamanager;
+
+import player.gamerunner.IGameRunner;
+
+public class LevelTransitioner {
+
+	private ILevelDataManager myLevelDataManager;
+	private IGameRunner myGameRunner;
+	
+	public LevelTransitioner(ILevelDataManager levelDataManager, IGameRunner gameRunner){
+		myLevelDataManager = levelDataManager;
+		myGameRunner = gameRunner;
+	}
+	
+	public int advanceLevel(){
+		myLevelDataManager.clear();
+		int advanceLevel = 1;
+	}
+	
+//	public replayLevel(){
+//		myLevelDataManager.getNextLevel();
+//	}
+	
+	
+	
+	
+}
