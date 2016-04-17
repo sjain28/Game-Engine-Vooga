a410d48c0ecd58a6a4eb54c74bf0349f7f8d83dd
Arjun Desai <arjunaashna@gmail.com>
2016-04-17 18:43:38 -0400

    fixed import/saving issue

--- voogasalad/authoring/gui/menubar/menuitems/NewImportArchetypeFileItem.java
+++ voogasalad/authoring/gui/menubar/menuitems/NewImportArchetypeFileItem.java
@@ -4,20 +4,33 @@ import authoring.gui.menubar.MenuItemHandler;
 import authoring.gui.menubar.builders.ImportArchetype;
 import authoring.interfaces.model.CompleteAuthoringModelable;
 import javafx.event.EventHandler;
+import player.gamedisplay.Menuable;
 import tools.VoogaException;
 
 public class NewImportArchetypeFileItem extends MenuItemHandler{
-    CompleteAuthoringModelable manager;
-    EventHandler event;
-    public NewImportArchetypeFileItem(CompleteAuthoringModelable model, EventHandler event){
-        this.manager=model;
+    private CompleteAuthoringModelable manager;
+    private EventHandler event;
+    public NewImportArchetypeFileItem(Menuable model, EventHandler event){
+        this.setManager((CompleteAuthoringModelable) model);
         this.event=event;
     }
     @Override
     public void handle () throws VoogaException {
-        ImportArchetype importArchetype = new ImportArchetype(manager);
+        ImportArchetype importArchetype = new ImportArchetype(getManager());
         importArchetype.show();
         
+    }
+    /**
+     * @return the manager
+     */
+    protected CompleteAuthoringModelable getManager () {
+        return manager;
+    }
+    /**
+     * @param manager the manager to set
+     */
+    private void setManager (CompleteAuthoringModelable manager) {
+        this.manager = manager;
     }
 
 }
--- voogasalad/authoring/gui/menubar/menuitems/SaveArchetypeFileItem.java
+++ voogasalad/authoring/gui/menubar/menuitems/SaveArchetypeFileItem.java
@@ -4,16 +4,17 @@ import authoring.gui.menubar.builders.ImportArchetype;
 import authoring.gui.menubar.builders.SaveArchetype;
 import authoring.interfaces.model.CompleteAuthoringModelable;
 import javafx.event.EventHandler;
+import player.gamedisplay.Menuable;
 
 public class SaveArchetypeFileItem extends NewImportArchetypeFileItem{
 
-    public SaveArchetypeFileItem (CompleteAuthoringModelable model, EventHandler event) {
+    public SaveArchetypeFileItem (Menuable model, EventHandler event) {
         super(model, event);
     }
     
     @Override
     public void handle(){
-        SaveArchetype saveArchetype = new SaveArchetype(manager);
+        SaveArchetype saveArchetype = new SaveArchetype(getManager());
         saveArchetype.show();
     }
 }
