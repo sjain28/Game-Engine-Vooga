abcc1a106a56213a9c0e81022bea3096210b68dd
Arjun Desai <arjunaashna@gmail.com>
2016-04-17 23:32:33 -0400

    Added VoogaFileChooser

--- voogasalad/authoring/gui/menubar/builders/ArchetypeBuilder.java
+++ voogasalad/authoring/gui/menubar/builders/ArchetypeBuilder.java
@@ -4,16 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import authoring.gui.items.NumberTextField;
 import authoring.interfaces.model.EditElementable;
-import authoring.model.VoogaFrontEndText;
-import authoring.resourceutility.VoogaFile;
-import authoring.resourceutility.VoogaFileType;
 import gameengine.Sprite;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.SelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -22,8 +14,12 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import tools.VoogaAlert;
+import tools.VoogaException;
+import tools.VoogaFileChooser;
 import tools.VoogaNumber;
 import tools.interfaces.VoogaData;
+
 
 public class ArchetypeBuilder extends Builder {
     private final int MASS_INDEX = 1;
@@ -44,7 +40,7 @@ public class ArchetypeBuilder extends Builder {
 
     private void makeProperty () {
         HBox complete = new HBox();
-        
+
         Text name = new Text("Property Name: ");
         name.setFill(Color.WHITE);
         TextField nameVal = new TextField();
@@ -52,56 +48,64 @@ public class ArchetypeBuilder extends Builder {
         value.setFill(Color.WHITE);
         NumberTextField valueVal = new NumberTextField();
         Button set = new Button("add");
-        set.setOnAction(e-> {
+        set.setOnAction(e -> {
             VoogaData val = new VoogaNumber();
-            try{
+            try {
                 val.setValue(Double.parseDouble(valueVal.getText()));
                 myProperties.put(nameVal.getText(), val);
             }
-            catch(Exception NumberFormatException){
+            catch (Exception NumberFormatException) {
                 numberError("Property Value");
             }
-            
+
             nameVal.clear();
             valueVal.clear();
         });
         complete.getChildren().addAll(name, nameVal, value, valueVal, set);
         this.getChildren().add(complete);
-        
+
     }
 
     private void makeImagePicker () {
-      HBox complete = new HBox();
-      Text label = new Text("Image");
-      label.setFill(Color.WHITE);
-      image = new Button("Choose Image");
-      iv = new ImageView();
-      image.setOnAction(e -> {
-              FileChooser fileChooser = new FileChooser();
-               
-              //Set extension filter
-              FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
-              FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
-              fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
-              
-              //Show open file dialog
-              java.io.File file = fileChooser.showOpenDialog(null);
-              myImagePath = file.getPath();
-              iv.setImage(new Image("file:///" + myImagePath));
-              iv.setPreserveRatio(true);
-              iv.setFitWidth(80);
-              System.out.println(myImagePath);
-      });
-      complete.getChildren().addAll(label, image, iv);
-      this.getChildren().add(complete);
-  }
-    
-    public void setImagePath(String path) {
-    	this.myImagePath = path;
-    	this.iv.setImage(new Image("file:///" + myImagePath));
-    	this.iv.setPreserveRatio(true);
-    	this.iv.setFitWidth(80);
-    	this.image.setDisable(true);
+        HBox complete = new HBox();
+        Text label = new Text("Image");
+        label.setFill(Color.WHITE);
+        image = new Button("Choose Image");
+        iv = new ImageView();
+        image.setOnAction(e -> {
+            VoogaFileChooser fileChooser = new VoogaFileChooser();
+
+            // Set extension filter
+            FileChooser.ExtensionFilter extFilterJPG =
+                    new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+            FileChooser.ExtensionFilter extFilterPNG =
+                    new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+            fileChooser.addFilters(extFilterJPG, extFilterPNG);
+
+            try {
+                myImagePath = fileChooser.launch();
+            }
+            catch (VoogaException e1) {
+                new VoogaAlert(e1.getMessage());
+            }
+            
+            System.out.println(myImagePath);
+            
+            iv.setImage(new Image("file:"+myImagePath));
+            iv.setPreserveRatio(true);
+            iv.setFitWidth(80);
+            
+        });
+        complete.getChildren().addAll(label, image, iv);
+        this.getChildren().add(complete);
+    }
+
+    public void setImagePath (String path) {
+        this.myImagePath = path;
+        this.iv.setImage(new Image("file:///" + myImagePath));
+        this.iv.setPreserveRatio(true);
+        this.iv.setFitWidth(80);
+        this.image.setDisable(true);
     }
 
     @Override
@@ -111,7 +115,8 @@ public class ArchetypeBuilder extends Builder {
             mass.setValue(Double.parseDouble(getInfo(MASS_INDEX)));
 
             getSpriteMaker().addArchetype(getInfo(),
-                                          new Sprite("file:///" + myImagePath, getInfo(), myProperties, mass));
+                                          new Sprite("file:///" + myImagePath, getInfo(),
+                                                     myProperties, mass));
             quit();
         }
         catch (Exception e) {
