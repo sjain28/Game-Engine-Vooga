91487780d75bd6070b19f35ef4feb90b4939e273
Aditya Srinivasan <as577@duke.edu>
2016-04-18 01:13:28 -0400

    Implemented number/boolean choice

--- voogasalad/authoring/gui/menubar/builders/PropertyBuilder.java
+++ voogasalad/authoring/gui/menubar/builders/PropertyBuilder.java
@@ -1,8 +1,13 @@
 package authoring.gui.menubar.builders;
 
+import authoring.CustomText;
+import authoring.gui.items.SwitchButton;
 import authoring.interfaces.model.EditElementable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -15,6 +20,8 @@ public class PropertyBuilder extends Builder {
 	private PropertiesTable pTable;
 	private TextField variableName;
 	private TextField variableValue;
+	private RadioButton number;
+	private HBox field;
 	
 	public PropertyBuilder() {
 		this(null);
@@ -30,9 +37,22 @@ public class PropertyBuilder extends Builder {
     
     private void populate() {
     	this.container = new VBox();
-    	container.getChildren().addAll(makeInfo("Property Name:", "Enter a name...", variableName),
+    	this.container.setSpacing(SPACING);
+    	this.container.setPadding(new Insets(SPACING));
+    	container.getChildren().addAll(makeDataSelector(),
+    								   makeInfo("Property Name:", "Enter a name...", variableName),
     								   makeInfo("Value:", "Enter a value...", variableValue),
     								   makeButtons());
+    }
+    
+    private HBox makeDataSelector() {
+    	ToggleGroup group = new ToggleGroup();
+    	RadioButton number = new RadioButton("Number");
+        number.setToggleGroup(group);
+        number.setSelected(true);
+        RadioButton bool = new RadioButton("Boolean");
+        bool.setToggleGroup(group);
+        return makeRow(number, bool);
     }
     
     public String getName() {
