d2ebaae40d8174d8f3c930193c6ea9cd5b54767a
Hunter Lee <narendly@gmail.com>
2016-04-18 20:15:33 -0400

    PlayerHelpHelpItem bug fixed and is working

--- voogasalad/authoring/gui/menubar/menuitems/GetPlayerHelpHelpItem.java
+++ voogasalad/authoring/gui/menubar/menuitems/GetPlayerHelpHelpItem.java
@@ -21,12 +21,11 @@ import tools.VoogaException;
 /**
  * Menu Item to select the board of the display
  * 
- * @author Nick
+ * @author Nick, Hunter
  *
  */
 public class GetPlayerHelpHelpItem extends PlayerMenuItemHandler {
-	//  private static final String HELP_URL =
-	//  "https://www.google.com/search?q=how+to+use+voogasalad&oq=how+to+use+voogasalad&aqs=chrome..69i57j69i60j69i65j69i60l3.2085j0j1&sourceid=chrome&ie=UTF-8";
+
 	private static final String HELP_VIDEO_URL = "https://www.youtube.com/watch?v=znMmrxpFKEs";
 	private static final String EXIT_MESSAGE = "Press Ctrl + Z to exit full screen mode";
 
@@ -39,7 +38,7 @@ public class GetPlayerHelpHelpItem extends PlayerMenuItemHandler {
 	 * @param model to interface backend interactions with the model
 	 * @param event: Unused vestige of previous poor programming. Should soon be phased out.
 	 */
-	public GetPlayerHelpHelpItem (CompleteAuthoringModelable model, EventHandler<InputEvent> event) {
+	public GetPlayerHelpHelpItem (Menuable model, EventHandler<InputEvent> event) {
 
 	}
 
@@ -50,23 +49,12 @@ public class GetPlayerHelpHelpItem extends PlayerMenuItemHandler {
 	 */
 	@Override
 	public void handle () throws VoogaException {
-		// TODO Auto-generated method stub
-		//if (Desktop.isDesktopSupported()) {
-		//  try {
-		//      Desktop.getDesktop().browse(new URI(HELP_URL));
-		//  }
-		//  catch (Exception e) {
-		//      throw new VoogaException();
-		//  }
-		//}
 
 		/**
 		 * Provides an instructional video on how to play the game
 		 * 
 		 */
 		popup(HELP_VIDEO_URL);
-
-
 	}
 
 
@@ -84,14 +72,11 @@ public class GetPlayerHelpHelpItem extends PlayerMenuItemHandler {
 		Stage stage = new Stage();        
 		vbox.getChildren().addAll(myBrowser);
 		VBox.setVgrow(myBrowser, Priority.ALWAYS);
-		//stage.setTitle(getResourceLoader().getString("PopupTitle"));
-		//stage.setWidth(POPUP_WIDTH);
-		//stage.setHeight(POPUP_HEIGHT);
 		stage.setScene(scene);
 		stage.setFullScreen(true);
 		stage.setFullScreenExitHint(EXIT_MESSAGE);
 		stage.setFullScreenExitKeyCombination
-		(new KeyCodeCombination(KeyCode.Z, KeyCombination.CONTROL_DOWN));
+					(new KeyCodeCombination(KeyCode.Z, KeyCombination.CONTROL_DOWN));
 		stage.show();
 	}
 
