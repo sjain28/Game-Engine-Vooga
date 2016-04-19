a9ff0d14df3fe49c2c9599ec87381f54f493d25f
Anita Desai <anita.desai96@gmail.com>
2016-04-18 20:59:00 -0400

    Added class to populate menu for event creation

---
+++ voogasalad/events/EventMenuPopulator.java
@@ -0,0 +1,28 @@
+package events;
+
+public class EventMenuPopulator {
+	// map of sprites by ID
+	// map of sprites by archetype
+	
+
+	public EventMenuPopulator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// get either cause types or effect types
+	// use reflection to get subclasses (CollisionCause, keyCause, etc)
+	
+	// based on cause/effect type, 
+	
+	// get global variables
+	
+	// get parameters of sprites based on archetype
+	
+	// get parameters of sprites based on ID
+	
+	// get methods for variable type
+	
+	// get parameter types
+	
+
+}
