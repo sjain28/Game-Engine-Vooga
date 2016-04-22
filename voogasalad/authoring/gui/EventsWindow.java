5b7c6b15e2de373d43907c4d09011bfa22edee48
NickLockett <NickS.Lockett@gmail.com>
2016-04-21 21:19:56 -0400

    event display

--- voogasalad/authoring/gui/EventsWindow.java
+++ voogasalad/authoring/gui/EventsWindow.java
@@ -13,10 +13,13 @@ import events.Effect;
 import events.VoogaEvent;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -66,9 +69,22 @@ public class EventsWindow extends TabPane implements Observer{
             ListView<String> causeList = new ListView<String>(causes.get(e));
             ListView<String> effectList = new ListView<String>(effects.get(e));
             info.getChildren().addAll(causeList, effectList);
+            info.setOnMouseClicked(new EventHandler(){
+                @Override
+                public void handle (Event event) {
+                    delete(info, e);
+                }
+            });
             content.getChildren().add(info);
         }
         
+    }
+
+    private void delete (HBox info, VoogaEvent e) {
+       myManager.getEvents().remove(e);
+       content.getChildren().remove(info);
+       causes.remove(e);
+       effects.remove(e);
     }
 
     @Override
