ecbaa380f6386265497cf3155d6118b701a94ff8
NickLockett <NickS.Lockett@gmail.com>
2016-04-24 10:42:00 -0400

    preliminary sharing

---
+++ voogasalad/authoring/gui/menubar/menuitems/EmailShareItem.java
@@ -0,0 +1,76 @@
+package authoring.gui.menubar.menuitems;
+
+import authoring.gui.menubar.MenuItemHandler;
+import player.gamedisplay.Menuable;
+import tools.VoogaException;
+import java.util.Properties;
+import javax.activation.*;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+
+public class EmailShareItem extends MenuItemHandler {
+    private Properties props;
+ 
+    public EmailShareItem(Menuable m){
+        super();
+        props = new Properties();
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.starttls.enable", true);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+    }
+
+    @Override
+    public void handle () throws VoogaException {
+       
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("from.mail.id@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("to.mail.id@gmail.com"));
+            message.setSubject("Testing Subject");
+            message.setText("PFA");
+
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+            Multipart multipart = new MimeMultipart();
+
+            messageBodyPart = new MimeBodyPart();
+            String file = "path of file to be attached";
+            String fileName = "attachmentName";
+            DataSource source = new FileDataSource(file);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(fileName);
+            multipart.addBodyPart(messageBodyPart);
+
+            message.setContent(multipart);
+
+            System.out.println("Sending");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+      }
+}
+
