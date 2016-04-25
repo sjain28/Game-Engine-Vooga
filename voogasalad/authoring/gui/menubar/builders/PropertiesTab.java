a410c6438c898df552faba1b5362c227749f9354
flashropower <flashropower@gmail.com>
2016-04-24 23:37:11 -0400

    fixed bug in adding properties

--- voogasalad/authoring/gui/menubar/builders/PropertiesTab.java
+++ voogasalad/authoring/gui/menubar/builders/PropertiesTab.java
@@ -5,37 +5,35 @@ import authoring.resourceutility.ButtonMaker;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import tools.VoogaNumber;
 import tools.interfaces.VoogaData;
 
 
 public class PropertiesTab extends VBox {
 
-    private PropertiesTable pTable;
+	private PropertiesTable pTable;
 
-    public PropertiesTab (Map<String, VoogaData> properties) {
-        pTable = new PropertiesTable(properties);
-        this.getChildren().addAll(pTable,
-                                  makeControl());
-    }
+	public PropertiesTab (Map<String, VoogaData> properties) {
+		pTable = new PropertiesTable(properties);
+		this.getChildren().addAll(pTable,
+				makeControl());
+	}
 
-    private HBox makeControl () {
-        HBox container = new HBox();
-        Button add = new ButtonMaker().makeButton("+", e -> {
-            PropertyBuilder pBuilder = new PropertyBuilder();
-            pBuilder.showAndWait();
-            System.out.println(pBuilder.getStatus());
-//            if(pBuilder.compile){
-//            System.out.println(pBuilder.getName() + "  jk  " + pBuilder.getValue());
-//            pTable.addVariableToTable(pBuilder.getName(), pBuilder.getValue());
-//            }
-        });
-        Button remove = new ButtonMaker().makeButton("-", e -> {
-            Property toRemove = pTable.getSelectionModel().getSelectedItem();
-            pTable.removeVariableFromTable(toRemove);
-        });
-        container.getChildren().addAll(add, remove);
-        return container;
-    }
+	private HBox makeControl () {
+		HBox container = new HBox();
+		Button add = new ButtonMaker().makeButton("+", e -> {
+			PropertyBuilder pBuilder = new PropertyBuilder();
+			pBuilder.showAndWait();
+			if (pBuilder.compileStatus() && pBuilder.getName() != null && pBuilder.getValue() != null) {
+				pTable.addVariableToTable(pBuilder.getName(), pBuilder.getValue());
+			}
+
+		});
+		Button remove = new ButtonMaker().makeButton("-", e -> {
+			Property toRemove = pTable.getSelectionModel().getSelectedItem();
+			pTable.removeVariableFromTable(toRemove);
+		});
+		container.getChildren().addAll(add, remove);
+		return container;
+	}
 
 }
