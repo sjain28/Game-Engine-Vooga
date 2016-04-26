62624d0c3edc7d6906c61b1e34664f267cd23808
Aditya Srinivasan <as577@duke.edu>
2016-04-26 19:34:29 -0400

    Refactored flow of setting up or playing a game

--- voogasalad/tools/VoogaFileChooser.java
+++ voogasalad/tools/VoogaFileChooser.java
@@ -9,67 +9,59 @@ import authoring.tagextension.GameTagManager;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
-public class VoogaFileChooser{
-    private static final String USER_RESOURCES_PATH = "user_resources/";
-    private static final String LOCAL_PATH= "voogasalad_DoovalSalad";
-    private GameTagManager tagManager;
-    private FileChooser fileChooser;
-    
-    public VoogaFileChooser(){
-    	tagManager = new GameTagManager();
-        fileChooser = new FileChooser();
-    }
-    
-    public String launch() throws VoogaException{
-    	String path = "";
-        File file = fileChooser.showOpenDialog(null);
-        if (isLocal(file)){
-            path = file.getPath().split(LOCAL_PATH)[1];
-            path=path.substring(1);
-            
-//            System.out.println("local: "+path);
-           // return path;
-        } else {
-            path= moveToLocalPath(file);
-            //return path;
-        }
-        tagManager.addTagsFromImage(path);
-        return path;
-    }
-    
-    public void setInitialDirectory(File file){
-        fileChooser.setInitialDirectory(file);
-    }
-    
-    public void addFilters(ExtensionFilter... filters){
-        fileChooser.getExtensionFilters().addAll(filters); 
-    }
-    
-    private boolean isLocal(File file){
-//    	System.out.println(file.getPath());
-        return file.getPath().contains(LOCAL_PATH);
-    }
-    
-    private String moveToLocalPath(File file) throws VoogaException{
-        String path = USER_RESOURCES_PATH+file.getName();
-        File newLocation = new File(path);
-        
-        if (newLocation.exists()){
-            throw new VoogaException("This filename already exits");
-        }
-        
-        try {
-            Files.copy(file.toPath(), newLocation.toPath(), StandardCopyOption.COPY_ATTRIBUTES);
-        }
-        catch (IOException e) {
-            throw new VoogaException("Could not import file");
-        }
+public class VoogaFileChooser {
+	
+	private static final String USER_RESOURCES_PATH = "user_resources/";
+	private static final String LOCAL_PATH = "voogasalad_DoovalSalad";
+	private GameTagManager tagManager;
+	private FileChooser fileChooser;
 
-        
-        
-        return path;
-    }
-    
-    
+	public VoogaFileChooser() {
+		tagManager = new GameTagManager();
+		fileChooser = new FileChooser();
+	}
+
+	public String launch() throws VoogaException {
+		String path = "";
+		File file = fileChooser.showOpenDialog(null);
+		if (isLocal(file)) {
+			path = file.getPath().split(LOCAL_PATH)[1];
+			path = path.substring(1);
+		} else {
+			path = moveToLocalPath(file);
+		}
+		tagManager.addTagsFromImage(path);
+		return path;
+	}
+
+	public void setInitialDirectory(File file) {
+		fileChooser.setInitialDirectory(file);
+	}
+
+	public void addFilters(ExtensionFilter... filters) {
+		fileChooser.getExtensionFilters().addAll(filters);
+	}
+
+	private boolean isLocal(File file) {
+		// System.out.println(file.getPath());
+		return file.getPath().contains(LOCAL_PATH);
+	}
+
+	private String moveToLocalPath(File file) throws VoogaException {
+		String path = USER_RESOURCES_PATH + file.getName();
+		File newLocation = new File(path);
+
+		if (newLocation.exists()) {
+			throw new VoogaException("This filename already exits");
+		}
+
+		try {
+			Files.copy(file.toPath(), newLocation.toPath(), StandardCopyOption.COPY_ATTRIBUTES);
+		} catch (IOException e) {
+			throw new VoogaException("Could not import file");
+		}
+
+		return path;
+	}
 
 }
