f28da32366074d4a65c49882d3376b05bccc5327
Aditya Srinivasan <as577@duke.edu>
2016-04-26 12:43:16 -0400

    Implemented adding of splines and lines

--- voogasalad/authoring/gui/menubar/builders/AnimationEventBuilder.java
+++ voogasalad/authoring/gui/menubar/builders/AnimationEventBuilder.java
@@ -1,26 +1,103 @@
 package authoring.gui.menubar.builders;
 
 import authoring.UIManager;
-import authoring.model.ElementManager;
+import authoring.VoogaScene;
+import authoring.gui.cartography.Connection;
+import authoring.interfaces.model.CompleteAuthoringModelable;
+import authoring.model.GameObject;
+import authoring.resourceutility.ButtonMaker;
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.CubicCurve;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.StrokeLineCap;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 import player.gamedisplay.Menuable;
+import resources.VoogaBundles;
 
 public class AnimationEventBuilder extends Stage {
 
-	private StackPane stack;
-	
+	private CompleteAuthoringModelable elManager;
+
+	private BezierCurve curve;
+	private Connection line;
+
+	private BorderPane border;
+	private Group stack;
+
 	public AnimationEventBuilder(Menuable model) {
-		ElementManager elManager = (ElementManager) ((UIManager) model).getManager();
-		for(Node element : elManager.getElements()) {
-			stack.getChildren().add(element);
-		}
-		Scene scene = new Scene(stack);
+		elManager = ((UIManager) model).getManager();
+		curve = new BezierCurve();
+		line = new Connection(0, 0, 100, 100);
+		initializePanes();
+		initializeScene();
+	}
+
+	private void initializePanes() {
+		stack = new Group();
+		border = new BorderPane();
+		// stack.setMinSize(Double.parseDouble(VoogaBundles.designboardProperties.getString("Width")),
+		// Double.parseDouble(VoogaBundles.designboardProperties.getString("Height")));
+		// for (Node element : elManager.getElements()) {
+		// GameObject go = ((GameObject) element);
+		// ImageView iv = new ImageView(go.getImage());
+		// iv.setLayoutX(go.getTranslateX());
+		// iv.setLayoutY(go.getTranslateY());
+		// iv.setFitWidth(go.getFitWidth());
+		// iv.setFitHeight(go.getFitHeight());
+		// stack.getChildren().add(iv);
+		// }
+		border.setTop(toolbar());
+		border.setCenter(stack);
+		border.setBottom(controls());
+	}
+
+	private void initializeScene() {
+		Scene scene = new VoogaScene(border, 700, 700);
 		this.setScene(scene);
 		this.show();
 	}
-	
+
+	private ToolBar toolbar() {
+		ToolBar toolbar = new ToolBar();
+		Button spline = new ButtonMaker().makeButton("Add Spline", e -> {
+			stack.getChildren().remove(line);
+			if (!stack.getChildren().contains(curve)) {
+				stack.getChildren().addAll(curve);
+			}
+		});
+		Button linear = new ButtonMaker().makeButton("Add Line", e -> {
+			stack.getChildren().remove(curve);
+			if (!stack.getChildren().contains(line)) {
+				stack.getChildren().addAll(line);
+			}
+		});
+		toolbar.getItems().addAll(spline, linear);
+		return toolbar;
+	}
+
+	private HBox controls() {
+		HBox row = new HBox();
+		row.getChildren().add(new ButtonMaker().makeButton("OK", e -> {
+			// TODO:
+			// Use animation factory to make the animation
+			this.close();
+		}));
+		return row;
+	}
+
 }
---
+++ voogasalad/authoring/gui/menubar/builders/BezierCurve.java
@@ -0,0 +1,56 @@
+package authoring.gui.menubar.builders;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.CubicCurve;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.StrokeLineCap;
+import javafx.scene.shape.StrokeType;
+
+public class BezierCurve extends Group {
+
+	private CubicCurve curve;
+
+	public BezierCurve() {
+		curve = createStartingCurve();
+		initializeControls();
+	}
+
+	CubicCurve createStartingCurve() {
+		curve = new CubicCurve();
+		curve.setStartX(100);
+		curve.setStartY(100);
+		curve.setControlX1(150);
+		curve.setControlY1(50);
+		curve.setControlX2(250);
+		curve.setControlY2(150);
+		curve.setEndX(300);
+		curve.setEndY(100);
+		curve.setStroke(Color.FORESTGREEN);
+		curve.setStrokeWidth(4);
+		curve.setStrokeLineCap(StrokeLineCap.ROUND);
+		curve.setFill(Color.CORNSILK.deriveColor(0, 1.2, 1, 0.6));
+		this.getChildren().add(curve);
+		return curve;
+	}
+
+	private void initializeControls() {
+		Line controlLine1 = new BoundLine(curve.controlX1Property(), curve.controlY1Property(), curve.startXProperty(),
+				curve.startYProperty());
+		Line controlLine2 = new BoundLine(curve.controlX2Property(), curve.controlY2Property(), curve.endXProperty(),
+				curve.endYProperty());
+		Anchor start = new Anchor(Color.PALEGREEN, curve.startXProperty(), curve.startYProperty());
+		Anchor control1 = new Anchor(Color.GOLD, curve.controlX1Property(), curve.controlY1Property());
+		Anchor control2 = new Anchor(Color.GOLDENROD, curve.controlX2Property(), curve.controlY2Property());
+		Anchor end = new Anchor(Color.TOMATO, curve.endXProperty(), curve.endYProperty());
+		this.getChildren().addAll(controlLine1, controlLine2, start, control1, control2, end);
+	}
+	
+	public CubicCurve getCurve() {
+		return this.curve;
+	}
+
+}
