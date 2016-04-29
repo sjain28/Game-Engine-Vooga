cd77b897fb04487494c2f725be801f9830dfc651
Krista Opsahl-Ong <kristaopsahlong@gmail.com>
2016-04-28 22:09:13 -0400

    integrating the database information throughout the classes

--- voogasalad/database/VoogaPlaySession.java
+++ voogasalad/database/VoogaPlaySession.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import tools.VoogaDate;
 import tools.VoogaNumber;
 
-public class VoogaPlaySession extends VoogaEntry{
+public class VoogaPlaySession extends VoogaSession{
 	public static final String DATE_PLAYED = "date_played";
 	public static final String PLAY_DURATION = "play_duration";
 	public static final String SCORE = "score";
@@ -17,7 +17,11 @@ public class VoogaPlaySession extends VoogaEntry{
 		setProperty(SCORE, new VoogaNumber(score));
 		setProperty(LEVEL_REACHED, new VoogaNumber(levelreached));
 	}
-	
+
+	@Override
+	protected void setDurationProperty(double gap) {
+		setProperty(PLAY_DURATION, new VoogaNumber(gap));		
+	}
 	public String toString(){
 		return getProperty(DATE_PLAYED).toString()+" "+getProperty(PLAY_DURATION).toString()+" "+getProperty(SCORE);
 	}
