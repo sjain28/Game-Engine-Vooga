9eea48dea7153686cf0edccdfcac919d118549e1
Krista Opsahl-Ong <kristaopsahlong@gmail.com>
2016-04-29 14:55:27 -0400

    fixed error that occured in savestats

--- voogasalad/stats/interaction/CurrentSessionStats.java
+++ voogasalad/stats/interaction/CurrentSessionStats.java
@@ -13,7 +13,8 @@ import tools.VoogaString;
 public class CurrentSessionStats {
 	private String myCurrentGame;
 	private String myCurrentUser;
-	private VoogaDataBase myDataBase;
+	private VoogaDataBase myDataBase = VoogaDataBase.getInstance();
+	
 	public void saveGameProgress(String levelurl){
 		getCurrentStatCell().setProperty(StatCell.LAST_SAVED_LEVEL_LOC, new VoogaString(levelurl));
 	}
