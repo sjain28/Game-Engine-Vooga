9a9af083b4ecc1d75e25cca0b743d2913047519a
Hunter Lee <narendly@gmail.com>
2016-04-30 16:43:54 -0400

    Edit Anchor.java

--- voogasalad/authoring/gui/menubar/builders/Anchor.java
+++ voogasalad/authoring/gui/menubar/builders/Anchor.java
@@ -7,11 +7,16 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.StrokeType;
 
 public class Anchor extends Circle {
+	
+	private static final int RADIUS = 10;
+	private static final double FILL_CONST = 0.5;
+	private static final int STROKE_WIDTH = 2;
+	
 	public Anchor(Color color, DoubleProperty x, DoubleProperty y) {
-		super(x.get(), y.get(), 10);
-		setFill(color.deriveColor(1, 1, 1, 0.5));
+		super(x.get(), y.get(), RADIUS);
+		setFill(color.deriveColor(1, 1, 1, FILL_CONST));
 		setStroke(color);
-		setStrokeWidth(2);
+		setStrokeWidth(STROKE_WIDTH);
 		setStrokeType(StrokeType.OUTSIDE);
 
 		x.bind(centerXProperty());
@@ -29,6 +34,10 @@ public class Anchor extends Circle {
 		setOnMouseReleased(e -> {
 			getScene().setCursor(Cursor.HAND);
 		});
+		enableMouseDrag(dragDelta);
+	}
+	
+	private void enableMouseDrag(Delta dragDelta) {
 		setOnMouseDragged(e -> {
 			double newX = e.getX() + dragDelta.x;
 			if (newX > 0 && newX < getScene().getWidth()) {
