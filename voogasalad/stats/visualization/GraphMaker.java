85ee91a4a30c1dae4b44e01472fff709812ddb79
Krista Opsahl-Ong <kristaopsahlong@gmail.com>
2016-04-30 02:03:21 -0400

    fixed the authoring session stufF

--- voogasalad/stats/visualization/GraphMaker.java
+++ voogasalad/stats/visualization/GraphMaker.java
@@ -17,13 +17,13 @@ public class GraphMaker {
 	private Object xPoint;
 	private Object yPoint;
 	//TODO: re-factor so that you only need to check the x/y param types once
-	public ScatterChart<?,?> createScatterPlot(String title, String xlabel, String ylabel, List<Object> xparam, List<Object> yparam){
+	public ScatterChart<?,?> createScatterPlot(String title, String xlabel, String ylabel, List<Object> xparams, List<Object> yparams){
 		//choose x axis type
-		if(xparam.get(0) instanceof Number){xAxis = new NumberAxis();}
+		if(xparams.get(0) instanceof Number){xAxis = new NumberAxis();}
 		else{xAxis = new CategoryAxis();}
 		
 		//choose y axis type
-		if(yparam.get(0) instanceof Number){yAxis = new NumberAxis();}
+		if(yparams.get(0) instanceof Number){yAxis = new NumberAxis();}
 		else{yAxis = new CategoryAxis();}
 		
 		//create the chart and get the title, x, and y axis
@@ -34,11 +34,15 @@ public class GraphMaker {
 		
 		//fill the series with x and y values
 		XYChart.Series series = new XYChart.Series();
-		for(int i = 0; i < xparam.size(); i++){
-			if(xparam.get(i) instanceof Number){ xPoint = Double.parseDouble(xparam.get(i).toString()); }
-			else{xPoint = xparam.get(i).toString(); }
-			if(yparam.get(i) instanceof Number){ yPoint = Double.parseDouble(yparam.get(i).toString()); }
-			else{yPoint = yparam.get(i).toString(); }
+		for(int i = 0; i < xparams.size(); i++){
+			System.out.println(i);
+			System.out.println(xparams.get(i).toString());
+			System.out.println(yparams.get(i).toString());
+
+			if(xparams.get(i) instanceof Number){ xPoint = Double.parseDouble(xparams.get(i).toString()); }
+			else{xPoint = xparams.get(i).toString(); }
+			if(yparams.get(i) instanceof Number){ yPoint = Double.parseDouble(yparams.get(i).toString()); }
+			else{yPoint = yparams.get(i).toString(); }
 			
 			series.getData().add(new XYChart.Data(xPoint,yPoint));
 		}
