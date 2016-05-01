0b3386718f35472516544a0d02ec96bbb3cc6078
flashropower <flashropower@gmail.com>
2016-04-30 21:54:12 -0400

    pushing before pulling

--- voogasalad/authoring/interfaces/AuthoringElementable.java
+++ voogasalad/authoring/interfaces/AuthoringElementable.java
@@ -2,8 +2,6 @@ package authoring.interfaces;
 
 import authoring.gui.Selector;
 import authoring.model.AuthoringElementableMenu;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.Menu;
 
 /**
  * This interface designates the elementable within the authoring environment (design board).
@@ -15,21 +13,6 @@ import javafx.scene.control.Menu;
  */
 
 public interface AuthoringElementable extends Elementable {
-//<<<<<<< HEAD
-//
-//	/**
-//	 * Gets the elementable selected within the authoring environment.
-//	 * @return
-//	 */
-//	public Elementable getElementable();
-//
-//	/**
-//	 * Selector to toggle the effect of the selected elementable in the
-//	 * authoring environment
-//	 * @param selector to affect the selected elementable 
-//	 */
-//	public void select(Selector selector);
-//=======
 	
     public Elementable getElementable();
     
