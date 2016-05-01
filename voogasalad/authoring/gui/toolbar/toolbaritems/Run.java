71368867bfc65945156431bdb6903ee638d7307b
NickLockett <NickS.Lockett@gmail.com>
2016-04-30 23:26:17 -0400

    fixed naming blocker

--- voogasalad/authoring/gui/toolbar/toolbaritems/Run.java
+++ voogasalad/authoring/gui/toolbar/toolbaritems/Run.java
@@ -10,19 +10,20 @@ import resources.VoogaBundles;
 public class Run extends ToolbarItemHandler {
 	Save save;
 	private UIManager model;
+	private String game;
 
 	public Run (Menuable model) {
 		//save = new Save(model);
 		this.model = (UIManager) model;
+		game = VoogaBundles.preferences.getProperty("GameName");
 	}
 
 	@Override
 	public void handle () {
 		GameRunner gameRunner = new GameRunner();
-		System.out.println("Running:"+"games/" + VoogaBundles.preferences.getProperty("GameName") 
-                                                        + "/levels/" + model.getManager().getName() + ".xml");
+		System.out.println("Running:"+"games/" + game + "/levels/" + model.getManager().getName() + ".xml");
 		//gameRunner.testLevel("games/" + VoogaBundles.preferences.getProperty("GameName") 
 							//+ "/levels/" + model.getManager().getName() + ".xml");
-		gameRunner.playGame(VoogaBundles.preferences.getProperty("GameName"));
+		gameRunner.playGame(game);
 	}
 }
