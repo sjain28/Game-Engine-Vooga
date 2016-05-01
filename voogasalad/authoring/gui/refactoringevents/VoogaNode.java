434afad30a6579ac8f6518f669c958ec30e75e51
flashropower <flashropower@gmail.com>
2016-05-01 02:40:18 -0400

    fixing up

--- voogasalad/authoring/gui/refactoringevents/VoogaNode.java
+++ voogasalad/authoring/gui/refactoringevents/VoogaNode.java
@@ -1,7 +1,17 @@
 package authoring.gui.refactoringevents;
 
+/**
+ * Class to return data if 
+ * 
+ * @author HarryGuo
+ *
+ */
 public interface VoogaNode {
     
+	/**
+	 * Returns a string of data
+	 * @return string of data based on what type of VoogaNode it is
+	 */
     public String getData();
     
 }
