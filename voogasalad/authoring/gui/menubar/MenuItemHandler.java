9cd27149ed8ff3db650d0265cbc16854417c1a38
flashropower <flashropower@gmail.com>
2016-05-01 00:55:36 -0400

    checking something out

--- voogasalad/authoring/gui/menubar/MenuItemHandler.java
+++ voogasalad/authoring/gui/menubar/MenuItemHandler.java
@@ -2,7 +2,17 @@ package authoring.gui.menubar;
 
 import tools.VoogaException;
 
+/**
+ * Abstract class to handle menubar functionality.
+ * 
+ * @author Aditya Srinivasan, Arjun Desai, Nick Lockett, Harry Guo
+ *
+ */
 public abstract class MenuItemHandler {
 	
+	/**
+	 * Handles the function of the menu item.
+	 * @throws VoogaException
+	 */
 	public abstract void handle() throws VoogaException;
 }
